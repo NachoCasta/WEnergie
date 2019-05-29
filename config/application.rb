c830bba3ed8bed00491bb33e2e13214b5ee485ca
Ignacio Castañeda <icastanedaw@gmail.com>
2019-05-29 02:53:48 -0400

    assets

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Example
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.js_compressor = Uglifier.new(harmony: true)
     config.assets.precompile += ['template_cotizacion.css']
 
     # Settings in config/environments/* take precedence over those specified here.
