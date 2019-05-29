b660a8675210b4d5409884dd82d4e982ddc9e955
Ignacio Castañeda <icastanedaw@gmail.com>
2019-05-29 02:29:39 -0400

    assets

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Example
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.precompile += ['template_cotizacion.css']
-    config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
-    config.assets.compile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
