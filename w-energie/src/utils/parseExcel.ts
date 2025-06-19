7256f1319541c682fa1a668e46d9224d05de45f2
Ignacio Castañeda <icasta@meta.com>
2025-06-18 17:34:34 -0700

    [excel] add console log for debugging

--- w-energie/src/utils/parseExcel.ts
+++ w-energie/src/utils/parseExcel.ts
@@ -13,6 +13,7 @@ export default async function parseExcel<RawData extends StringKeyedObject, Data
   const workbook = XLSX.read(arrayBuffer);
   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
   const rawData = XLSX.utils.sheet_to_json<RawData>(worksheet);
+  console.log("parseExcel/rawData", rawData);
   const data = rawData.map(normalizeRow).map(parseData);
   return data;
 }
