a88d6383591f1a9817275ee9251e8df786ba362d
Ignacio Castañeda <icasta@meta.com>
2025-06-18 17:32:43 -0700

    [fix] normalize headers

--- w-energie/src/utils/parseExcel.ts
+++ w-energie/src/utils/parseExcel.ts
@@ -1,6 +1,11 @@
 import XLSX from "xlsx";
+import _ from "lodash";
 
-export default async function parseExcel<RawData, Data>(
+type StringKeyedObject = {
+  [key: string]: any;
+}
+
+export default async function parseExcel<RawData extends StringKeyedObject, Data>(
   file: File,
   parseData: (rawData: RawData) => Data
 ): Promise<Array<Data>> {
@@ -8,6 +13,10 @@ export default async function parseExcel<RawData, Data>(
   const workbook = XLSX.read(arrayBuffer);
   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
   const rawData = XLSX.utils.sheet_to_json<RawData>(worksheet);
-  const data = rawData.map(parseData);
+  const data = rawData.map(normalizeRow).map(parseData);
   return data;
+}
+
+function normalizeRow<RawData extends StringKeyedObject>(row: RawData): RawData {
+  return _.mapKeys(row, (v, k) => k.trim()) as any
 }
